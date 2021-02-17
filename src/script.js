d7dc5da4171661f1bd11b9b41162d313153bbcf6
Dharanidharan Mani <iamdharanidharan@gmail.com>
2021-02-17 13:04:26 +0530

    Removed gConnection.js and modified UI for log out

--- src/script.js
+++ src/script.js
@@ -1,3 +1,97 @@
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '384015590213-mer7mo6grfo36sss9nt9rhne2m13nlbv.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyD3_bp1ztPy8JqejfUJIqXPT6chJUfgRZg';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = 'https://mail.google.com/ ' + 'https://www.googleapis.com/auth/gmail.addons.current.action.compose ' + 'https://www.googleapis.com/auth/gmail.compose ' + 'https://www.googleapis.com/auth/gmail.modify ' + 'https://www.googleapis.com/auth/gmail.send';
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+var tableDiv = document.getElementById('table-Div');
+var contentDiv = document.getElementById('contents-Div');
+var composeBtn = document.getElementById('compose-button');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function initClient() {
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+    }, function (error) {
+        appendPre(JSON.stringify(error, null, 2));
+    });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton.style.display = 'none';
+        signoutButton.style.display = 'block';
+        tableDiv.style.display = '';
+        contentDiv.style.display = '';
+        composeBtn.style.display = 'block';
+        getPrimaryMsgIDs();
+        
+    } else {
+        authorizeButton.style.display = 'block';
+        signoutButton.style.display = 'none';
+        tableDiv.style.display = 'none';
+        contentDiv.style.display = 'none';
+        composeBtn.style.display = 'none';
+    }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+    var pre = document.getElementById('content');
+    var textContent = document.createTextNode(message + '    ');
+    pre.appendChild(textContent);
+}
 
 
 
@@ -47,7 +141,7 @@ function readSubjectAndSnippets(msgIds) {
     tableBody.id = 'tableBody';
     table.append(tableBody);
     resultDiv.append(table);
-    let i=0;
+    
     for (val in msgIds) {
         gapi.client.gmail.users.messages.get({
             'userId': 'me',
@@ -90,7 +184,7 @@ function readSubjectAndSnippets(msgIds) {
                     button.setAttribute('onclick',"readPrimary('"+data1.result.id+"')");
                     
                    // console.log(msgIds[i]);
-                    i++;
+                   
                     td = document.createElement('td');
                     td.append(button);
                     tr.append(td);
@@ -219,29 +313,45 @@ function readPrimary(messageID) {
     
 }
 
-function sendMail() {
-    var toAddress = document.getElementById('compose-to').value;
-    var subjectText = document.getElementById('compose-subject').value;
-    var messageText = document.getElementById('compose-message').value;
-    const message =
-        "From: iamdharanidharan@gmail.com\r\n" +
-        "To: " + toAddress +"\r\n" +
-        "Subject: " + subjectText +"\r\n\r\n" +
-        messageText;
+function customSendMail() {
+
+    gapi.client.gmail.users.getProfile({
+        'userId': 'me',
+    }).then(res => {
+        return res;
+    }).then(data1 => {
+        var fromAddress = data1.result.emailAddress;
+        var toAddress = document.getElementById('compose-to').value;
+        var subjectText = document.getElementById('compose-subject').value;
+        var messageText = document.getElementById('compose-message').value;
+        console.log(fromAddress);
+       const message =
+            "From: "+ fromAddress +"\r\n" +
+            "To: " + toAddress +"\r\n" +
+            "Subject: " + subjectText +"\r\n\r\n" +
+            messageText;
+    
+    
+        // The body needs to be base64url encoded.
+        const encodedMessage = btoa(message)
+    
+        const reallyEncodedMessage = encodedMessage.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
+        //console.log(reallyEncodedMessage);
+        gapi.client.gmail.users.messages.send({
+            userId: 'me',
+            resource: {
+                // same response with any of these
+                raw: reallyEncodedMessage
+                // raw: encodedMessage
+                // raw: message
+            }
+        }).then(cAlert('Mail Sent!!')); 
+
+    })    
+}
 
 
-    // The body needs to be base64url encoded.
-    const encodedMessage = btoa(message)
-
-    const reallyEncodedMessage = encodedMessage.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
-    console.log(reallyEncodedMessage);
-    gapi.client.gmail.users.messages.send({
-        userId: 'me',
-        resource: {
-            // same response with any of these
-            raw: reallyEncodedMessage
-            // raw: encodedMessage
-            // raw: message
-        }
-    }).then(function () { console.log("Mail sending done!") });
+let cAlert = (message)=> {
+    document.getElementById('myalert-message').innerHTML = message;
+    document.getElementById('myalert').style.display = 'block';
 }
